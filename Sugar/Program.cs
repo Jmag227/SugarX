b77e93375a33331c848db6e9073dabfbdc63c86b
Jarom Magalei <jarom.pro.magalei@gmail.com>
2021-12-09 08:31:43 -0700

    first commit'

---
+++ Sugar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var num1 = 4;
+            var answer = 4 < 9 ? $"{num1} Is less than 9 " : "Is equal or greater than 9";
+            Console.WriteLine(answer);
+        }
+    }
+}
